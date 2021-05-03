feaa6d1326fc23bfaa3a11050746b3772a28a1a2
cybersystem1327 <82329110+cybersystem1327@users.noreply.github.com>
2021-05-03 19:39:29 -0400

    Add files via upload

---
+++ Mac_Change.py
@@ -0,0 +1,19 @@
+import pyfiglet
+import os
+ascii_banner = pyfiglet.figlet_format("MAC CHANGE ")
+print(ascii_banner)
+print("Version 1.0 ")
+
+x = int(input("\nMerhaba \n\nMac Adrresi Değiştirmek İçin 1'e Basın \nEski Mac addresiniz için 2' ye Basın  :"))
+if x == 1 :
+    os.system("ifconfig wlan0 down")
+    os.system("macchanger -r wlan0")
+    os.system("ifconfig wlan0 up")
+    print("Mac Adresiniz Değiştirildi")
+elif x == 2 :
+    os.system("ifconfig wlan0 down")
+    os.system("macchanger -p wlan0")
+    os.system("ifconfig wlan0 up")
+    print("Mac Eski Haline Getirildi")
+else :
+    print("yanlış komut girdiniz")
